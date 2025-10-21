e818bdec1a6b06823635a5a23dadcd3611041501
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-10-21 19:28:10 +0000

    first Commit

---
+++ src/main/java/com/tricol/App.java
@@ -0,0 +1,13 @@
+package com.tricol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello charaf eddine !" );
+    }
+}
